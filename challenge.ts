434bb73ef458219add7a4ee09b7fb9d18a6961d5
mujib2953 <ansarimujiburrehman@gmail.com>
2020-02-27 14:38:40 +0530

    TS challenge.

--- challenge.ts
+++ challenge.ts
@@ -29,3 +29,38 @@ class ShipmentSearchIndex {
 interface ShipmentUpdateListenerInterface {
     receiveUpdate(id: string, shipmentData: any)
 }
+
+
+// ---------------------------
+// class Shipment extends ShipmentSearchIndex implements ShipmentUpdateListenerInterface {
+class Shipment implements ShipmentUpdateListenerInterface {
+
+    private shipmentQueue: string[];
+
+    constructor(private shipmentSearchIndex: ShipmentSearchIndex) {
+        this.shipmentQueue = [];
+    }
+
+    async receiveUpdate(id: string, shipmentData:  any) {
+
+        if (this.shipmentQueue.includes(id)) {
+            // --- this id is present in the queue so not to proceed.
+            console.info(`Sorry, This shipment: ${id} is already in the queue`);
+            return;
+        }
+
+        // --- Adding new shipment to process
+        this.shipmentQueue.push(id);
+
+        try {
+            const time = await this.shipmentSearchIndex.updateShipment(id, shipmentData);
+            console.info(`Shipment updated for ${id}. and ${shipmentData}`);
+        } catch(e) {
+            console.info(`Shipment updated unsuccessfull for ${id} -  ${shipmentData}.`, e);
+        } finally {
+            // --- we have to update the queue after every success/error in updateShipment
+            this.shipmentQueue = this.shipmentQueue.filter(ids => ids !== id);
+            console.log("Queue is updated.");
+        }
+    }
+}
